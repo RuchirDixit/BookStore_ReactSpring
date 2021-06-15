190f7458be2c1ff2588df8e7309b2de4acdab388
RuchirDixit <ruchirtd96@gmail.com>
2021-06-16 00:32:39 +0530

    [Ruchir]Added: Book store apis

--- src/main/java/com/bridgelabz/bookstore/controller/BookController.java
+++ src/main/java/com/bridgelabz/bookstore/controller/BookController.java
@@ -1,14 +1,22 @@
 package com.bridgelabz.bookstore.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bridgelabz.bookstore.dto.BookDTO;
+import com.bridgelabz.bookstore.entity.BookEntity;
 import com.bridgelabz.bookstore.service.IBookService;
 import com.bridgelabz.bookstore.util.Response;
 
@@ -27,10 +35,47 @@ public class BookController {
 	 * @param bookDto
 	 * @return
 	 */
-	@PostMapping("/addBook")
-	public ResponseEntity<Response> addBook(@RequestBody BookDTO bookDto){
+	@PostMapping("/addBook/{token}")
+	public ResponseEntity<Response> addBook(@RequestBody BookDTO bookDto,@PathVariable String token){
 		log.debug("Add new book");
-		Response response = bookService.addNewBook(bookDto);
+		Response response = bookService.addNewBook(token,bookDto);
+		return new ResponseEntity<Response>(response, HttpStatus.OK);
+	}
+	
+	/**
+	 * To get all books
+	 * @param token : JWT to authorize user
+	 * @return ResponseEntity<List<?>>
+	 */
+	@GetMapping("/getBooks/{token}")
+	public ResponseEntity<List<?>> getAllBooks(@PathVariable String token){
+		log.debug("Get all books");
+		List<BookEntity> books = bookService.getAllBooks(token);
+		return new ResponseEntity<List<?>>(books, HttpStatus.OK);	
+	}
+	
+	/**
+	 * Update book details
+	 * @param bookDto : Book details to update
+	 * @param token : JWT to authorize user
+	 * @return ResponseEntity<Response>
+	 */
+	@PutMapping("/updateBook/{token}")
+	public ResponseEntity<Response> updateBook(@RequestBody BookDTO bookDto,@PathVariable String token){
+		log.debug("Update book");
+		Response response = bookService.updateBook(token,bookDto);
+		return new ResponseEntity<Response>(response, HttpStatus.OK);
+	}
+	
+	/**
+	 * Delete book
+	 * @param token : JWT to authorize user
+	 * @return ResponseEntity<Response>
+	 */
+	@DeleteMapping("/deleteBook/{token}")
+	public ResponseEntity<Response> deleteBook(@PathVariable String token,@RequestParam("name") String bookName){
+		log.debug("Delete book");
+		Response response = bookService.deleteBook(token,bookName);
 		return new ResponseEntity<Response>(response, HttpStatus.OK);
 	}
 }
--- src/main/java/com/bridgelabz/bookstore/service/IBookService.java
+++ src/main/java/com/bridgelabz/bookstore/service/IBookService.java
@@ -1,5 +1,23 @@
 package com.bridgelabz.bookstore.service;
 
+import java.util.List;
+
+import com.bridgelabz.bookstore.dto.BookDTO;
+import com.bridgelabz.bookstore.entity.BookEntity;
+import com.bridgelabz.bookstore.util.Response;
+
 public interface IBookService {
+
+	// To add new book to store
+	Response addNewBook(String token, BookDTO bookDto);
+
+	// Get all books
+	List<BookEntity> getAllBooks(String token);
+
+	// To update book details
+	Response updateBook(String token, BookDTO bookDto);
+
+	// To delete user
+	Response deleteBook(String token, String bookName);
 
 }
